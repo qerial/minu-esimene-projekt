6e47035d74559dddf663eb67621fdbebb8aa4273
Opilane TTHK <opilane@tthk.ee>
2025-10-24 09:18:51 +0300

    Lisatud Projekt ja gitignore

---
+++ UusProjekt/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace UusProjekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Head päeva!");
+        }
+    }
+}
